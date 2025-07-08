59040d715fe7bc9de606cbb1f47a29859f4cc220
Shivam Tiwari <shivamtiwarix88@gmail.com>
2025-07-09 00:40:15 +0530

    frontend init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      {"Add text area and submit button with loading effect"}
+      {"Can use react form action and useFormStatus hook for loading effect while backend request"}
+    </div>
+  );
+}
